7805a590922c6ecedadcb4fd81086c46db217c10
Rehulina Kabeakan <rehulinakabeakan@gmail.com>
2026-02-16 21:47:33 +0700

    added DetailTugasSesi9.js

---
+++ Tugas_Sesi9/Test/DetailTugasSesi9.js
@@ -0,0 +1,5 @@
+"Tugas Sesi 9 - Web UI Automation Fundamental"
+
+1. Buatlah 1 project dengan Tech Stack seperti pada sesi ini
+2. Automate website saucedemo.com dengan case seperti berikut: Sukses Login & Urutkan Produk dari A-Z
+3. Push ke Github masing-masing dan submit link Github ke LMS
---
+++ Tugas_Sesi9/Test/README.md
@@ -0,0 +1,7 @@
+Digitalskola_Tugas_Sesi9
+
+Title -> Sesi 09 - API Testing
+Homework -> API Testing
+Author -> Rehulina Kabeakan
+Reviewer -> Mr Luki
+Homework Status -> Done
